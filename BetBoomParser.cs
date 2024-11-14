8339ba777f4682f91cc917a348d7366c2c28737b
RaffailAmbrozioKusto <91315300+ErmilovAlexander234@users.noreply.github.com>
2024-11-14 18:30:58 +0300

    Create BetBoomParser.cs

---
+++ BetBoomParser.cs
@@ -0,0 +1,16 @@
+namespace bb_ne_budet;
+
+public class BetBoomParser
+{
+    public async Task<List<MatchInfo>> ParseMatchesAsync() 
+    {
+        // парсинг матчей
+        throw new NotImplementedException();
+    }
+
+    public async Task<Dictionary<string, double>> ParseMatchCoefficientsAsync(string matchUrl)
+    {
+        // парсинга кэфов
+        throw new NotImplementedException();
+    }
+}
